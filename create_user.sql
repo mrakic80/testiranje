4fe42337e0bf13902fc7b7ad4a64ff546b4512fb
mrakic80 <milan.rakic80@gmail.com>
2015-07-08 14:08:16 +0200

    Add three initial values

--- create_user.sql
+++ create_user.sql
@@ -4,5 +4,20 @@ id int NOT NULL AUTO_INCREMENT,
 username varchar(100),
 password varchar(100),
 PRIMARY KEY (id)
-)
+);
+
+INSERT INTO Users
+(username, password)
+VALUES
+('Marko', 'passmarko');
+
+INSERT INTO Users
+(username, password)
+VALUES
+('Darko', 'passdarko');
+
+INSERT INTO Users
+(username, password)
+VALUES
+('Zarko', 'passzarko');
 
